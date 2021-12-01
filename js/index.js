5c0b0b2ffe6397a2daf1338214821ed042b051d8
Manguberdifrontdev <95344067+Manguberdifrontdev@users.noreply.github.com>
2021-12-01 04:32:35 -0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(".count").counterUp({
+	delay:10,	
+	time:3000
+}) 
+
+mybutton = document.getElementById('myBtn');
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	}else{
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
